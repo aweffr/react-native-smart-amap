320b3b78d1f6199449e1913097a4b9f687a2560b
aweffr <aweffr@qq.com>
2019-05-18 18:53:24 +0800

    format code

--- AMap-ios.js
+++ AMap-ios.js
@@ -5,79 +5,71 @@
  * Copyright (c) 2016 react-native-component <moonsunfall@aliyun.com>
  */
 
-import React, {
-    Component,
-} from 'react'
-import {
-    View,
-    requireNativeComponent,
-    NativeModules,
-    findNodeHandle,
-    Platform,
-} from 'react-native'
+import React, {Component} from 'react'
+import {View, requireNativeComponent, NativeModules, findNodeHandle, Platform} from 'react-native'
 import PropTypes from "prop-types";
 
-const AMapManager = Platform.OS == 'ios' ? NativeModules.AMap : null
+const AMapManager = Platform.OS === 'ios' ? NativeModules.AMap : null;
+const NativeAMap = Platform.OS === 'ios' ? requireNativeComponent('RCTAMap', AMap) : View;
 
 export default class AMap extends Component {
 
-    static constants = {
-        userTrackingMode: Platform.OS == 'ios' ? AMapManager.userTrackingMode : null, //only for ios
-    }
+  static constants = {
+    userTrackingMode: Platform.OS === 'ios' ? AMapManager.userTrackingMode : null, //only for ios
+  };
 
-    static defaultProps = {
-        //mapType: 0,
-        //showTraffic: false,
-        //showsUserLocation: true,
-    }
+  static defaultProps = {
+    //mapType: 0,
+    //showTraffic: false,
+    //showsUserLocation: true,
+  };
 
-    static propTypes = {
-        ...View.propTypes,
-        options: PropTypes.shape({
-            frame: PropTypes.shape({
-                width: PropTypes.number.isRequired,
-                height: PropTypes.number.isRequired,
-            }),
-            mapType: PropTypes.number,
-            showTraffic: PropTypes.bool,
-            showsUserLocation: PropTypes.bool,
-            userTrackingMode: PropTypes.number,
-            centerCoordinate: PropTypes.shape({
-                latitude: PropTypes.number.isRequired,
-                longitude: PropTypes.number.isRequired,
-            }),
-            zoomLevel: PropTypes.number,
-            centerMarker: PropTypes.string,
-        }),
-        onDidMoveByUser: PropTypes.func,
-    }
+  static propTypes = {
+    ...View.propTypes,
+    options: PropTypes.shape({
+      frame: PropTypes.shape({
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+      }),
+      mapType: PropTypes.number,
+      showTraffic: PropTypes.bool,
+      showsUserLocation: PropTypes.bool,
+      userTrackingMode: PropTypes.number,
+      centerCoordinate: PropTypes.shape({
+        latitude: PropTypes.number.isRequired,
+        longitude: PropTypes.number.isRequired,
+      }),
+      zoomLevel: PropTypes.number,
+      centerMarker: PropTypes.string,
+    }),
+    onDidMoveByUser: PropTypes.func,
+  };
 
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-    render() {
-        return (
-            <NativeAMap
-                {...this.props}
-            />
-        )
-    }
+  render() {
+    return (
+      <NativeAMap
+        {...this.props}
+      />
+    )
+  }
 
-    setOptions(options) {
-        AMapManager.setOptions(findNodeHandle(this), options)
-    }
+  setOptions(options) {
+    AMapManager.setOptions(findNodeHandle(this), options)
+  }
 
-    searchPoiByCenterCoordinate(params) {
-        AMapManager.searchPoiByCenterCoordinate(params) //传null为默认参数配置
-    }
+  searchPoiByCenterCoordinate(params) {
+    AMapManager.searchPoiByCenterCoordinate(params) //传null为默认参数配置
+  }
 
-    setCenterCoordinate(coordinate) {
-        //console.log('findNodeHandle => ')
-        //console.log(findNodeHandle)
-        AMapManager.setCenterCoordinate(findNodeHandle(this), coordinate)
-    }
+  setCenterCoordinate(coordinate) {
+    //console.log('findNodeHandle => ')
+    //console.log(findNodeHandle)
+    AMapManager.setCenterCoordinate(findNodeHandle(this), coordinate)
+  }
 }
 
-const NativeAMap = Platform.OS == 'ios' ? requireNativeComponent('RCTAMap', AMap) : View
